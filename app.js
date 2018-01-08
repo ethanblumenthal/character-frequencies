cdedfc09ea26db45d7581caf4ca2316c6f959450
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-01-08 11:51:12 -0800

    added submit event handler

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+d3.select('form')
+    .on('submit', function() {
+        d3.event.preventDefault();
+        var input = d3.select('input');
+        var text = d3.property('value');
+    });
+
 function getFrequencies(str) {
     var sorted = str.split('').sort();
     var data = [];
