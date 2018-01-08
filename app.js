8d5937beb4ba68320d15b381a5ef9d2792bd3df3
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-01-08 11:52:54 -0800

    added letter dom manipulation

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ d3.select('form')
         d3.event.preventDefault();
         var input = d3.select('input');
         var text = d3.property('value');
+
+    d3.select('#letter')
+        .selectAll('.letter')
+        .data(getFrequencies(text))
+        .enter()
+        .append('div')
+            .classed('letter', true)
+            .style('width', '20px')
+            .style('line-height', '20px')
+            .style('margin-right', '5px')
+            .style('height', function(d) {
+                return d.count * 20 + 'px';
+            })
+            .text(function(d) {
+                return d.character;
+            });
     });
 
 function getFrequencies(str) {
