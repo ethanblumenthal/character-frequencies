32467ac5dba1b34b5d968465d5b0d97a6f8de7d6
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-01-08 11:44:47 -0800

    added getFrequencies function

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write your code here!
+function getFrequencies(str) {
+    var sorted = str.split('').sort();
+    var data = [];
+    for (var i = 0; i < sorted.length; i++) {
+        var last = data[data.length - 1];
+        if (last && last.character === sorted[i]) last.count++;
+        else data.push({ character: sorted[i], count: 1 });
+    }
+    return data;
+}
